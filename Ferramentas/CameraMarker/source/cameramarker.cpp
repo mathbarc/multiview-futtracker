163e5f12d435ec3281406f744b592f1ae4cc892e
Matheus Barcelos de Oliveira <matheusbarcelosoliveira@gmail.com>
2016-09-08 23:56:27 -0300

    Correção

--- Ferramentas/CameraMarker/source/cameramarker.cpp
+++ Ferramentas/CameraMarker/source/cameramarker.cpp
@@ -1,6 +1,6 @@
 #include "cameramarker.h"
 #include "ui_cameramarker.h"
-#include <QMessageBox.h>
+#include <QMessageBox>
 CameraMarker::CameraMarker(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::CameraMarker)
