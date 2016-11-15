80180c5f605ed2f3b7a0722b9cedeab79069b933
Matheus Barcelos de Oliveira <matheusbarcelosoliveira@gmail.com>
2016-11-15 02:27:01 -0200

    Alteração banco

--- DataBase/database.sql
+++ DataBase/database.sql
@@ -1,2 +1,6 @@
 create database if not exists futtracker;
 
+drop table if exists players;
+create table players( name varchar(150),
+                      shirtNumber int,
+                      id int);
