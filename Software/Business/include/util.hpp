70f077d496b79fb52bf65997dc2ffe21cef27c0f
Matheus <matheusbarcelosoliveira@gmail.com>
2017-02-27 12:36:19 -0300

    Alterações

---
+++ Software/Business/include/util.hpp
@@ -0,0 +1,10 @@
+#ifndef UTIL_HPP
+#define UTIL_HPP
+
+#include <opencv2/core/core.hpp>
+#include <QImage>
+
+QImage cvMatToQImage(const cv::Mat3b& img);
+
+
+#endif
---
+++ Software/Business/source/util.cpp
@@ -0,0 +1,6 @@
+#include "util.hpp"
+
+QImage cvMatToQImage(const cv::Mat3b& img)
+{
+    return QImage();
+}
